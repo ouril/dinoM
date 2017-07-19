202853c8bed102522af717ba6306928127cda1ef
ouril <ourilkregg@gmail.com>
2017-07-19 18:22:50 +0300

    добавленна первая сортировка, пока через функцию

--- dinomania/urls.py
+++ dinomania/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from dinomania.views import MainView, NewsView, BooksView, NewsDetail
+from dinomania.views import MainView, NewsView, BooksView, NewsDetail,sort_for_time
 
 urlpatterns = [
 
     url(r'^$', MainView.as_view(), name='main'),
     url(r'^news$', NewsView.as_view(), name='news'),
-    url(r'^books$', BooksView.as_view(), name='books'),
+    url(r'^news/sorted/$', sort_for_time, name='sort'),
+    url(r'^books/$', BooksView.as_view(), name='books'),
     url(r'^news/(?P<pk>\d+)/$', NewsDetail.as_view(), name='news_detail')
 ]
--- dinomania/views.py
+++ dinomania/views.py
@@ -7,17 +7,32 @@ from django.views.generic.detail import DetailView
 class MainView(ListView):
     template_name = "index.html"
     model = New
-    paginate_by = 3    
+    paginate_by = 3
+    queryset = New.objects.order_by("time").reverse()[:3]
 
-    def get_context_data(self, **kwargs):
-        context = super(MainView, self).get_context_data(**kwargs)
-        context['object_list'] = New.objects.order_by("time").reverse()[:3]
-        return context
+   # def get_context_data(self, **kwargs):
+  #      context = super(MainView, self).get_context_data(**kwargs)
+  #      context['object_list'] = New.objects.order_by("time").reverse()[:3]
+  #      return context
 
 class NewsView(ListView):
     template_name = "news.html"
     paginate_by = 12
     model = New
+    queryset = New.objects.all().order_by("time").reverse()
+
+def sort_for_time(request):
+    context = New.objects.all().order_by("time").reverse() if request.GET.get('reversed') else New.objects.all().order_by(
+        "time")
+    return render(request, 'news.html', {"object_list": context})
+''''
+class SortedNewsView(ListView):
+    template_name = "news.html"
+    paginate_by = 12
+    model = New
+    queryset = New.objects.all().order_by("time").reverse() if request.get('reversed') else New.objects.all().order_by("time").reverse()
+'''
+
 
 class BooksView(NewsView):
     model = Book
